ea4f7084ff143c836ac5247afb7eec4749975487
gopaladhikari <gopuadks@gmail.com>
2024-01-19 19:31:26 +0545

    updated app.ts for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { Response } from "express";
 
 const app = express();
+
+app.get("/", (_, res: Response) => {
+  res.json({ message: "Hi from express!" });
+});
 
 export { app };
